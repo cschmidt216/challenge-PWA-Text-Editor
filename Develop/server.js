a0568f375c810854f5cbfff7633a2bcc99c28afc
cschmidt216 <iamdrainy@gmail.com>
2023-07-10 15:32:50 -0700

    fixed routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,7 @@ app.use(express.static('client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('routes/htmlRoutes.js')(app);
+const htmlRoutes = require(path.join(__dirname, 'routes', 'htmlRoutes.js'));
+htmlRoutes(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
